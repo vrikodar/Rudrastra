56e864be8766dd0f5d48d70f355f4bfc07c25476
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-12 23:01:00 -0500

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -5,9 +5,12 @@
 #https://github.com/SxNade
 #CONTRIBUTE
 
+echo [*]Resetting all the changes made......
 iptables --flush --table nat
 iptables --flush FORWARD
 echo 0 > /proc/sys/net/ipv4/ip_forward
+echo [*]Deleting Configuration Files.....
 rm hostapd.conf
 rm dnsmasq.conf
 rm fakehosts.conf
+echo [+]The Reset Was Successfully Completed...
