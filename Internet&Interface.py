163a2ea65bfd4a9e43eb6aea525273d437371c39
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-19 23:44:09 -0500

    Update Internet&Interface.py

--- Internet&Interface.py
+++ Internet&Interface.py
@@ -9,12 +9,11 @@ import sys
 #importing the required Libraries...
 
 gol = '''
- ____  _   _ ____  ____      _    ____ _____ ____      _              __ ____  
-|  _ \| | | |  _ \|  _ \    / \  / ___|_   _|  _ \    / \             \ \\ \ \ 
-| |_) | | | | | | | |_) |  / _ \ \___ \ | | | |_) |  / _ \ _____ ____(_) |\ \ \
-|  _ <| |_| | |_| |  _ <  / ___ \ ___) || | |  _ <  / ___ \_____|_____|| |/ / /
-|_| \_\\___/|____/|_| \_\/_/   \_\____/ |_| |_| \_\/_/   \_\         (_) /_/_/ 
-                                                                      /_/     '''
+    //   ) )                                                                    
+   //___/ /            ___   /  __      ___      ___    __  ___  __      ___    
+  / ___ (   //   / / //   ) / //  ) ) //   ) ) ((   ) )  / /   //  ) ) //   ) ) 
+ //   | |  //   / / //   / / //      //   / /   \ \     / /   //      //   / /  
+//    | | ((___( ( ((___/ / //      ((___( ( //   ) )  / /   //      ((___( (     '''
 
 print(gol)
 
@@ -23,7 +22,7 @@ if len(sys.argv) != 3:
 	sys.exit(0)
 
 
-print(colored("[+]\nThis Script will Configure The IP Tables Rules and interfaces.....!", "green", attrs=['bold']))
+print(colored("\n[+] This Script will Configure The IP Tables Rules and interfaces.....!", "green", attrs=['bold']))
 
 #Capturing the user input for Interfaces to be used in EVIL-TWIN
 inter_ifc = sys.argv[1]
@@ -36,12 +35,12 @@ mon_ifc = ap_ifc + "mon"
 def forward(inter_ifc, ap_ifc):
     print(colored("[+]Preparing The FAKE-AP interface..!", "green", attrs=['bold']))
     os.system(f'airmon-ng start {ap_ifc} > /dev/null')
-    print(colored("Configuring the internet Access..!", "green", attrs=['bold']))
+    print(colored("\n\nConfiguring the internet Access..!", "green", attrs=['bold']))
     os.system(f'ifconfig {mon_ifc} up 192.168.1.1 netmask 255.255.255.0')
     os.system(f'route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1')
     os.system(f'iptables --table nat --append POSTROUTING --out-interface {inter_ifc} -j MASQUERADE')
     os.system(f'iptables --append FORWARD --in-interface {mon_ifc} -j ACCEPT')
-    print(colored("Enabling IP Forwarding NOW.....!", "green", attrs=['bold']))
+    print(colored("\nEnabling IP Forwarding NOW.....!", "green", attrs=['bold']))
     os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
     print(colored("[*]\n\nEverything SET....NOW you can run the Fake AP using start-ap file", "red", attrs=['bold']))
 
